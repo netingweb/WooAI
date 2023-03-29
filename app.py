2ae5af6110605437d6cd9d375b8adf7dea6f8773
Neting S.r.l <netingweb@gmail.com>
2023-03-29 14:44:18 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,17 @@
+import flask
+from flask import request
+
+app = flask.Flask(__name__)
+
+@app.route('/generate', methods=['GET'])
+@app.route('/', methods=['GET'])
+
+def home():
+    return 'Welcome to WooAI API'
+
+def generate():
+    req = []
+    args = request.args['type', 'title', 'description', 'description_lenght', 'excerpt', 'slug', 'meta_description', 'og_schema', 'image_alt', 'hints']
+    for arg in args
+        req.append(arg)
+    return req
