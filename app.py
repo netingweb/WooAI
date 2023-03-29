a5771392ddb2e3b233b3e159587fdf28adae2729
Neting S.r.l <netingweb@gmail.com>
2023-03-29 16:13:34 +0200

    bug fix

--- app.py
+++ app.py
@@ -8,10 +8,3 @@ app = flask.Flask(__name__)
 
 def home():
     return 'Welcome to WooAI API'
-
-def generate():
-    req = []
-    args = request.args['type', 'title', 'description', 'description_lenght', 'excerpt', 'slug', 'meta_description', 'og_schema', 'image_alt', 'hints']
-    for arg in args:
-        req.append(arg)
-    return req
